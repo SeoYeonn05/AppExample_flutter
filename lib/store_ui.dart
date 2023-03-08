c29f54b90096a9f555c754b1a11497d80110436a
SeoYeonn05 <seoyeonn051@gmail.com>
2023-03-09 00:09:01 +0900

    fix: main과 store_ui 분류, recipe_ui 생성

---
+++ lib/store_ui.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:storeapp/utils/snackbar.dart';
+
+enum Category { Shoes, Bags, Clothes, Accessory }
+
+
+class StoreMain extends StatelessWidget {
+  const StoreMain({Key? key}) : super(key: key);
+
+
+  @override
+  Widget build(BuildContext context) {
+    var msg = {
+
+    };
+
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(25.0),
+            child: Row(
+              children: [
+                TextButton(
+                  onPressed: () {
+                    showSnackBar(context, "msg");
+                  },
+                  child: const Text("Shoes",
+                      style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black)),
+                ),
+                Spacer(),
+                Text("Bags", style: TextStyle(fontWeight: FontWeight.bold)),
+                Spacer(),
+                Text("Clothes", style: TextStyle(fontWeight: FontWeight.bold)),
+                Spacer(),
+                Text("Accessory",
+                    style: TextStyle(fontWeight: FontWeight.bold)),
+              ],
+            ),
+          ),
+          Expanded(child: Image.asset("assets/images/bag.jpg", fit: BoxFit.fill)),
+          const SizedBox(height: 2),
+          Expanded(child: Image.asset("assets/images/bag2.jpg", fit: BoxFit.fill)),
+        ],
+      ),
+    ));
+  }
+}
+
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:storeapp/main.dart';
+import 'package:storeapp/store_ui.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
