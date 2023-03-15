2059df051905382ca5620cf477cb864b16258be1
seoyeon <seoyeonn051@gmail.com>
2023-03-15 22:57:54 +0900

    fix: recipeTitle 위젯 생성

--- lib/recipe_ui.dart
+++ lib/recipe_ui.dart
@@ -1,12 +1,62 @@
 
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class RecipeUi extends StatelessWidget {
   const RecipeUi({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(fontFamily:"PatuaOne"),
+      home: recipePage()
+    );
   }
+
+  Widget recipePage(){
+    return Container(
+      child: Column(
+        children: [
+          recipeTitle(),
+          recipeMenu()
+        ],
+      )
+    );
+  }
+
+  Widget recipeTitle(){
+    return const Padding(
+      padding: EdgeInsets.only(top: 20),
+      child: Text(
+        "Recipes",
+        style: TextStyle(fontSize: 30)
+      ),
+    );
+  }
+
+  Widget recipeMenu() {
+    return Row(
+      children: [
+
+      ],
+    );
+  }
+
+  Widget recipeItem(){
+    return Container(
+      child: Column(
+        children: [
+
+        ],
+      )
+    );
+  }
+
+
 }
+
+
+
+
